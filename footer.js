c469480208fd3f1c07c1d6adb68dabb7cf16e99f
Oleh Zaporozhchenko <thepr0stre1@gmail.com>
2025-07-05 18:00:11 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const footer = document.createElement("footer");
+  
   footer.style.backgroundColor = "#f1f1f1";
   footer.style.padding = "20px";
   footer.style.textAlign = "center";
   footer.style.marginTop = "50px";
-  footer.innerHTML = `
-    <p>Made with ❤️ by Neura</p>
-  `;
+  
+  footer.innerHTML = `<p>Made with ❤️ by Neura</p>`;
+  
   document.body.appendChild(footer);
 });

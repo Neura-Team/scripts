694884a10932d3ac2f4086716d59d36bd71e2f20
Oleh Zaporozhchenko <thepr0stre1@gmail.com>
2025-07-05 17:54:59 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,13 +1,11 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const footer = document.createElement("footer");
-    footer.style.backgroundColor = "#f1f1f1";
-    footer.style.padding = "20px";
-    footer.style.textAlign = "center";
-    footer.style.marginTop = "50px";
-    footer.innerHTML = `
-      <p>Made with ❤️ by Neura</p>
-    `;
-    document.body.appendChild(footer);
-  });
-</script>
+document.addEventListener("DOMContentLoaded", function () {
+  const footer = document.createElement("footer");
+  footer.style.backgroundColor = "#f1f1f1";
+  footer.style.padding = "20px";
+  footer.style.textAlign = "center";
+  footer.style.marginTop = "50px";
+  footer.innerHTML = `
+    <p>Made with ❤️ by Neura</p>
+  `;
+  document.body.appendChild(footer);
+});
